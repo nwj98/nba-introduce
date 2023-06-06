4eded9e02929da4b317816c8046e6a9c9ffc2c17
nwj98 <woojin7095@gmail.com>
2023-06-07 01:56:26 +0900

    fix: first_name값이 ''인 경우가 있어서 수정

--- app.py
+++ app.py
@@ -14,18 +14,24 @@ nba_teams = teams.get_teams()
 nba_players = players.get_players()
 
 # http://127.0.0.1:5000/ or localhost:5000/
+
+
 @app.route('/')
 def index():
     # index.html에 category를 넘겨주는 역할
     return render_template("index.html", categorys=category)
 
 # http://127.0.0.1:5000/teams or localhost:5000/teams
+
+
 @app.route('/teams')
 def teams():
     # teams.html에 category를 넘겨주는 역할
     return render_template('teams.html', teams=nba_teams)
 
 # http://127.0.0.1:5000/players or localhost:5000/players
+
+
 @app.route('/players', methods=["GET", "POST"])
 def players():
     search_nba_player = {}
@@ -36,11 +42,16 @@ def players():
         last_name = request.form['last_name']
 
         # 입력값과 일치하는 선수를 players.html에 전달
-        if not (full_name == '' and first_name == '' and last_name == ''):
-            for player in nba_players:
-                if player["full_name"] == full_name or player["first_name"] == first_name or player["last_name"] == last_name:
-                    search_nba_player = player
-                    break
+        for player in nba_players:
+            if player["full_name"] == full_name:
+                search_nba_player = player
+                break
+            elif player["first_name"] == first_name and first_name != '':
+                search_nba_player = player
+                break
+            elif player["last_name"] == last_name:
+                search_nba_player = player
+                break
 
     return render_template('players.html', players=search_nba_player)
 
