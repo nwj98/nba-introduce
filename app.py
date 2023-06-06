d1eac9992d18e96b42b75bf75b403e9ec01458c2
nwj98 <woojin7095@gmail.com>
2023-06-07 00:29:32 +0900

    feat: index.html에 category링크 달기, nba-api를 이용하여 nba-team 데이터 가져오기

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template
+from nba_api.stats.static import teams
 
 app = Flask(__name__)
 
+# category
+category = ['teams', 'players', 'careers']
+
+# nba-teams json
+nba_teams = teams.get_teams()
+
 @app.route('/')
 def index():
-    return render_template("index.html")
+    # index.html에 category를 넘겨주는 역할
+    return render_template("index.html", categorys=category)
 
 if __name__ == '__main__':
     app.run(debug=True)
