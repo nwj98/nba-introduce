b5dcc351fea72dc87ab222cf261da758fc7c1e1c
nwj98 <woojin7095@gmail.com>
2023-06-07 02:08:31 +0900

    feat: nba-api를 통해 커리어를 가져오고, careers.html에서 입력받은 값으로 선수를 조회하고 커리어 기록을 가져옴

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 from nba_api.stats.static import teams, players
+from nba_api.stats.endpoints import playercareerstats
+import pandas as pd
 
 # http://127.0.0.1:5000 or localhost:5000
 app = Flask(__name__)
@@ -54,6 +56,49 @@ def players():
                 break
 
     return render_template('players.html', players=search_nba_player)
+
+
+@app.route('/careers', methods=["GET", "POST"])
+def careers():
+    # careers.html에서 입력받은 값을 가져옴
+    search_nba_player = {}
+    if request.method == 'POST':
+        full_name = request.form['full_name']
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+
+        # 입력값과 일치하는 선수를 careers.html에 전달
+        for player in nba_players:
+            if player["full_name"] == full_name:
+                search_nba_player = player
+                break
+            elif player["first_name"] == first_name and first_name != '':
+                search_nba_player = player
+                break
+            elif player["last_name"] == last_name:
+                search_nba_player = player
+                break
+    
+    career = pd.DataFrame()
+    # 검색된 선수가 존재 할 때
+    if search_nba_player != {}:
+        # 검색된 선수 id를 이용하여 선수 커리어 DataFrame 생성
+        career = playercareerstats.PlayerCareerStats(
+            player_id=search_nba_player['id']).get_data_frames()[0]
+        career.rename(columns={'PLAYER_ID': 'PLAYER_NAME'}, inplace=True)   # 기존 DataFrame Column 이름 변경
+        career.rename(columns={'TEAM_ID': 'TEAM_NAME'}, inplace=True)       # 기존 DataFrame Column 이름 변경
+        career.drop(['LEAGUE_ID'], axis=1, inplace=True)                    # 기존 DataFrame Column 삭제
+        # PLAYER_ID로 되어있던 값을 선수 이름으로 변경
+        for player in nba_players:
+            if player['id'] == search_nba_player['id']:
+                career.loc[career['PLAYER_NAME'] == search_nba_player['id'],
+                           'PLAYER_NAME'] = player['full_name']
+                break
+        # TEAM_ID로 되어있던 값을 팀 이름으로 변경
+        for team in nba_teams:
+            career.loc[career['TEAM_NAME'] == team['id'],
+                       'TEAM_NAME'] = team['full_name']
+    return render_template('careers.html', players=search_nba_player, tables=[career.to_html(classes='data')], titles=career.columns.values)
 
 
 if __name__ == '__main__':
