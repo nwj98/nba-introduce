87dab0a39fdbb5affcaa7dc27e875931f72aaef7
nwj98 <woojin7095@gmail.com>
2023-06-07 00:36:42 +0900

    feat: nba-teams 데이터를 teams.html에 넘김

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from nba_api.stats.static import teams
 
+# http://127.0.0.1:5000 or localhost:5000
 app = Flask(__name__)
 
 # category
@@ -9,10 +10,18 @@ category = ['teams', 'players', 'careers']
 # nba-teams json
 nba_teams = teams.get_teams()
 
+# http://127.0.0.1:5000/ or localhost:5000/
 @app.route('/')
 def index():
     # index.html에 category를 넘겨주는 역할
     return render_template("index.html", categorys=category)
+
+# http://127.0.0.1:5000/teams or localhost:5000/teams
+@app.route('/teams')
+def teams():
+    # teams.html에 category를 넘겨주는 역할
+    return render_template('teams.html', teams=nba_teams)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
